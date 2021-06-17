19e624f093009812e203d6320d628ffb460a06a3
Cedrik <cedrik.git@tryninja.io>
2021-06-17 01:19:11 -0500

    fix: types.graphql not found

--- src/graphql/server.ts
+++ src/graphql/server.ts
@@ -1,9 +1,10 @@
 import { readFileSync } from 'fs';
+import {join} from 'path';
 import { ApolloServer, Config, gql } from 'apollo-server-koa';
 import { connection } from '../db/connection';
 import { resolvers } from './resolvers';
 
-const typeDefs = gql(readFileSync(`${process.cwd()}/types.graphql`, 'utf8'));
+const typeDefs = gql(readFileSync(join(__dirname, 'types.graphql'), 'utf8'));
 
 export function graphServer(opts: Config = {}) {
   return new ApolloServer({
