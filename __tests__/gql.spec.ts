ca6bf69aee320ca2aa9c0d7b0948f8343d092b17
kouraf <farouk.yeddou@gmail.com>
2021-09-15 20:54:55 +0100

    fix BigNumber

--- __tests__/gql.spec.ts
+++ __tests__/gql.spec.ts
@@ -5,18 +5,13 @@ describe('TRANSACTION', () => {
   it('gets 10 txs', async () => {
     const data = 'test';
     let i = 12;
-    // while (i--)
-    await createTransaction(blockweave, data + i);
+    while (i--) await createTransaction(blockweave, `${data} : ${i}`);
+
     const res1 = (await ardb.search('transactions').find()) as any;
+    const res2 = (await ardb.next()) as any;
 
-    expect(res1.length).toEqual(1);
-  });
-
-  it('gets a tx', async () => {
-    const data = 'test';
-    const id = await createTransaction(blockweave, data);
-    const res = (await ardb.search('transaction').id(id).find()) as any;
-    expect(res.id).toEqual(id);
+    expect(res1.length).toEqual(10);
+    expect(res2.length).toEqual(2);
   });
 });
 
