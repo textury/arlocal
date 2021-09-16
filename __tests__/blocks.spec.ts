40dcbd394ff655eccb5e31c93f4cefe1041df01a
Cedrik <cedrik.git@tryninja.io>
2021-09-16 13:16:38 -0500

    fix: test index_hash

--- __tests__/blocks.spec.ts
+++ __tests__/blocks.spec.ts
@@ -2,10 +2,13 @@ import { blockweave } from '../test-setup';
 
 describe('BLOCK', () => {
   it('GET /block/hash/:indep_hash', async () => {
-    // const block = await blockweave.blocks.getCurrent();
+    await mine();
     const { current: block } = await blockweave.network.getInfo();
-
-    const indepHash = await blockweave.blocks.get(block);
-    expect(indepHash).toBe(block);
+    const b = await blockweave.blocks.get(block);
+    expect(b.indep_hash).toBe(block);
   });
 });
+
+async function mine() {
+  await blockweave.api.get('mine');
+}
