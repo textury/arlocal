41fb0ab29c733b2499e6d0219c5a6f80bf81129f
Cedrik <cedrik.git@tryninja.io>
2021-11-04 07:10:58 -0600

    1.1.8

--- src/db/wallet.ts
+++ src/db/wallet.ts
@@ -31,7 +31,7 @@ export class WalletDB {
 
   async getWalletBalance(address: string): Promise<number> {
     const wallet = await this.getWallet(address);
-    if(!wallet) return 0
+    if (!wallet) return 0;
     return wallet.balance;
   }
 
